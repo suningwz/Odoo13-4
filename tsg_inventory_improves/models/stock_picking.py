bd4a5b8481f7fe4d85b40a2d94070b0573e1b86b
jguapacho <jjguapacho@gmail.com>
2021-01-28 14:24:02 -0500

    28JAN21 - TSG Addons Translate Improves

--- tsg_inventory_improves/models/stock_picking.py
+++ tsg_inventory_improves/models/stock_picking.py
@@ -6,6 +6,7 @@ class Picking(models.Model):
     _inherit = "stock.picking"
 
     project_id = fields.Many2one(comodel_name='project.project', string="Project")
+    attachment_ids = fields.Many2many(comodel_name='ir.attachment', relation='atthacments_for_pickings',column1='picking_id', column2='att_id',string='Attachments')
 
     @api.onchange('partner_id')
     def oncahnge_partner_owner(self):
